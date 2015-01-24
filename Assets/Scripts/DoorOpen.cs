8cd55d1cadbf80a8c8ec828fcb5674fc5b44aee2
Gareth Rothwell <garethrothwell95@gmail.com>
2015-01-24 12:49:43 +0000

    Door Lowering and Raising

--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -2,16 +2,20 @@
 using System.Collections;
 
 public class DoorOpen : MonoBehaviour {
-
+	private Vector3 offset;
 	// Use this for initialization
 	void Start () {
-	
+		offset = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (GameObject.Find("PlayerController").GetComponent<PlayerController>().triggerOn) {
-				transform.Rotate(new Vector3 (0, 1, 0));
+			transform.position = offset - (new Vector3 (0,5,0));
 			}
+
+		if (!GameObject.Find("PlayerController").GetComponent<PlayerController>().triggerOn) {
+			transform.position = offset;
+		}
 	}
 }
