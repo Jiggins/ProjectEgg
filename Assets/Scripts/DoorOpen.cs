2ce09193cadaeaff2bc402991ba4229ad77c8977
Gareth Rothwell <garethrothwell95@gmail.com>
2015-01-24 12:09:23 +0000

    Update on switches and triggers

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.Find("PlayerController").GetComponent<PlayerController>().triggerOn) {
+				transform.Rotate(new Vector3 (0, 1, 0));
+			}
+	}
+}
