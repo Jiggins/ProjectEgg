f5c7bd07c16d96e0c0ed66e1741eed19cf4b4876
Jack Higgins <jackhiggins07@gmail.com>
2015-01-25 14:27:35 +0000

    Level One 100% complete

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	// public GameObject player;
+	public float smoothTime = 0.7f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+	public bool canMove = true;
+
+
+	// Update is called once per frame
+	void LateUpdate () {
+		if (canMove) {
+			Vector3 point = camera.WorldToViewportPoint (target.position);
+			Vector3 delta = target.position - camera.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, point.z));
+
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp (transform.position, destination, ref velocity, smoothTime);
+		}
+	}
+
+	public void SetAbilityToMove(bool move) {
+		this.canMove = move;
+	}
+}
---
+++ Assets/Scripts/Camera/NoFollowZone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoFollowZone : MonoBehaviour {
+
+	void OnTriggerEnter(Collider colide) {
+		if (colide.gameObject.tag == "Player") {
+					
+			CameraController temp = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>();
+			temp.canMove = false;
+			}
+	}
+
+	void OnTriggerExit(Collider colide) {
+		if (colide.gameObject.tag == "Player") {
+			
+			CameraController temp = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>();
+			temp.canMove = true;
+		}
+	}
+}
